f08573686770867a79b47b426725792e1210401f
And4Web <anands2807@gmail.com>
2022-01-23 10:07:05 -0800

    css done.

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,9 +2,10 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    <div>
-      <h1>React Navbar</h1>
-      <button>Click</button>
+    <div className="navbar">
+      <button>
+        <i class="fas fa-bars"></i>
+      </button>
       <ul>
         <li>Home</li>
         <li>About</li>
