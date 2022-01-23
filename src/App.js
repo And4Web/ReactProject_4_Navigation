fba5f9a18b8a1ddd6fd6f04e5b2c9f005fdf4f02
And4Web <anands2807@gmail.com>
2022-01-23 09:43:42 -0800

    basic layout created.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Navbar from "./Navbar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello..</h1>
+      <Navbar />
     </div>
   );
 }
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>React Navbar</h1>
+      <button>Click</button>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
